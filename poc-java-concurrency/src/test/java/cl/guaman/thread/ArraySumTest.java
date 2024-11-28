cac183b136b19e60a3a60d54cf4a023faadb4402
Fabián Guamán <fguamanf@gmail.com>
2024-11-28 16:33:18 -0300

    - Se agrega ejemplo de suma en 2 Thread

---
+++ poc-java-concurrency/src/test/java/cl/guaman/thread/ArraySumTest.java
@@ -0,0 +1,62 @@
+package cl.guaman.thread;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains a unit test for calculating the sum of an integer array
+ * using multiple threads.
+ *
+ * @author fguaman
+ */
+public class ArraySumTest {
+    /**
+     * The array to be summed.
+     */
+    private final int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    /**
+     * The total sum calculated by both threads.
+     */
+    private int totalSum = 0;
+
+    /**
+     * Test method to calculate the sum of the array using two threads.
+     *
+     * @throws InterruptedException if any thread is interrupted while waiting.
+     */
+    @Test
+    void execute() throws InterruptedException {
+        Thread threadOne = new Thread(() -> {
+            int sum = 0;
+            for (int i = 0; i < array.length / 2; i++) {
+                sum += array[i];
+            }
+            synchronized (ArraySumTest.class) {
+                totalSum += sum;
+            }
+        });
+
+        Thread threadTwo = new Thread(() -> {
+            int sum = 0;
+            for (int i = array.length / 2; i < array.length; i++) {
+                sum += array[i];
+            }
+            synchronized (ArraySumTest.class) {
+                totalSum += sum;
+            }
+        });
+
+        // start both threads
+        threadOne.start();
+        threadTwo.start();
+
+        // wait for both threads to complete
+        threadOne.join();
+        threadTwo.join();
+
+        // verify that the total sum is correct
+        Assertions.assertEquals(55, totalSum);
+        System.out.println("totalSum = " + totalSum);
+    }
+}
