f22b3dc52d01a036a3083a9cb8d332728a3c7595
Fabián Guamán <fguamanf@gmail.com>
2024-11-30 11:15:28 -0300

    [poc-java-concurrency] se agrega caso 3 de synchronized

---
+++ poc-java-concurrency/src/main/java/cl/guaman/threads/case_iii/CounterSync.java
@@ -0,0 +1,33 @@
+package cl.guaman.threads.case_iii;
+
+/**
+ * CounterSync is a thread-safe class that provides synchronized methods
+ * to increment and retrieve the value of a shared counter.
+ *
+ * @author fguaman
+ */
+public class CounterSync {
+    private int counter = 0;
+
+    /**
+     * Default constructor for CounterSync.
+     */
+    public CounterSync() {
+    }
+
+    /**
+     * Increments the counter in a thread-safe manner.
+     */
+    public synchronized void increment() {
+        counter++;
+    }
+
+    /**
+     * Retrieves the current value of the counter in a thread-safe manner.
+     *
+     * @return the current value of the counter.
+     */
+    public synchronized int getCounter() {
+        return counter;
+    }
+}
---
+++ poc-java-concurrency/src/test/java/cl/guaman/threads/case_iii/CounterSyncTest.java
@@ -0,0 +1,55 @@
+package cl.guaman.threads.case_iii;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Test class for CounterSync to verify thread-safe increment functionality.
+ *
+ * @author fguaman
+ */
+class CounterSyncTest {
+
+    /**
+     * Logger instance to log test results and information.
+     */
+    private static final Logger logger = Logger.getLogger(CounterSyncTest.class.getName());
+
+    /**
+     * Executes a multi-threaded test to increment a shared counter using CounterSync.
+     * Ensures the counter is incremented accurately in a thread-safe manner.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting for tasks to complete.
+     */
+    @Test
+    void executeIncrementWithThreads() throws InterruptedException {
+        CounterSync counterSync = new CounterSync();
+        int numberOfThreads = 10;
+        int incrementsPerThread = 1000;
+
+        CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            executorService.execute(() -> {
+                for (int j = 0; j < incrementsPerThread; j++) {
+                    counterSync.increment();
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+
+        Assertions.assertEquals(numberOfThreads * incrementsPerThread, counterSync.getCounter());
+        logger.log(Level.INFO, "counter={0}", numberOfThreads * incrementsPerThread);
+    }
+}
